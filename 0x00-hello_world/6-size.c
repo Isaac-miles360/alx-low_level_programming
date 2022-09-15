44765f9dc1d9791c4e7059cae32913bb33ec0b77
Isaac-miles360 <isaacdavidmiles29@gmail.com>
2022-09-15 09:54:21 -0700

    will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	char charType;
+	float floatType;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType);
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType);
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longintType);
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longlongintType);
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType);
+	
+	return (0);
+}
